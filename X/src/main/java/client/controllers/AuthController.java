c61e5f63ddaa87e0afff8403ec4af62da7d0073f
Arshida_0 <arshidaazadvari@gmail.com>
2026-07-12 23:41:36 +0430

    connecting some controllers

--- X/src/main/java/client/controllers/AuthController.java
+++ X/src/main/java/client/controllers/AuthController.java
@@ -1,4 +1,36 @@
 package client.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
 public class AuthController {
+
+    @FXML private TextField emailField;
+    @FXML private TextField usernameField;
+    @FXML private TextField passwordField;
+    @FXML private TextArea registerError;
+    @FXML private TextArea loginError;
+
+    @FXML
+    public void initialize(){
+        //
+    }
+    
+    @FXML
+    public void signup() {
+        //
+    }
+    
+    @FXML
+    public void login() {
+        //
+    }
+    
+    @FXML
+    public void switchScene() {
+        //
+    }
+
 }
