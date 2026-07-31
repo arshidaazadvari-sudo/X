1c6a0f94bf9418d6ad102633c35ff9b713e68a87
ZahraSadatMirvakili <zahrasadatmirvakili13@gmail.com>
2026-07-31 15:55:30 +0330

    edit2

--- X/src/main/java/server/database/daos/FollowDAO.java
+++ X/src/main/java/server/database/daos/FollowDAO.java
@@ -12,6 +12,10 @@ import java.util.List;
 
 public class FollowDAO {
     public boolean follow(int followerId, int followeeId){
+        if (followerId == followeeId){
+            System.out.println("You cannot follow your self");
+            return false;
+        }
 
         if (isFollowing(followerId, followeeId)){
             System.out.println("Already following this user");
